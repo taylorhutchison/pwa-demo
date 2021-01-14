c01226d33946cf7d3f04f12add4d793c0d6f5c87
Taylor Hutchison <seanthutchison@gmail.com>
2021-01-13 19:30:13 -0600

    Add functions code.

--- functions/ConfigurationProviderHttpTrigger.cs
+++ functions/ConfigurationProviderHttpTrigger.cs
@@ -13,21 +13,13 @@ namespace PWADemo.Function
     public static class ConfigurationProviderHttpTrigger
     {
         [FunctionName("ConfigurationProviderHttpTrigger")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var responseMessage = "Hello, world!";
 
             return new OkObjectResult(responseMessage);
         }
